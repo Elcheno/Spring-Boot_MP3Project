b292c1b24bd420848ae99f273d30d9e37165db36
Ruben <rubenca2312@gmail.com>
2023-06-05 15:34:42 +0200

    Gulp terminado

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Proyecto para 3º Trimestre del Grado Superior 1º DAM<br>
 Realizado con Spring-Boot y Maven<br>
 Consiste en una Web estilo Spotify<br>
+<br>
+
+# Comandos de GULP
+gulp run -> Iniciar el proyecto<br>
+gulp build_style -> Compilar SASS<br>
+gulp build_style_watch -> Compilar SASS en modo --watch<br>
+gulp build_style_doc -> Generar SASSDOC<br>
+gulp(default) -> Compila SASS y Inicia el proyecto<br>
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,41 @@
-const {src, dest, series, parallel} = require('gulp');
+const {src, dest, series, start} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sassdoc = require('sassdoc');
+const run = require('gulp-run');
+const watch = require('gulp-watch');
 
 
-function build_css() {
+
+function build_style() {
     return src("src/main/resources/static/sass/style.scss")
         .pipe(sass())
         .pipe(dest("src/main/resources/static/css/"));
 }
 
-function build_sass_doc(){
-    return src("src/main/resources/static/sass/*.scss")
-        .pipe(sassdoc());
-        // .pipe(dest("docs"));
+function build_style_watch() {
+    build_style();
+    return watch("src/main/resources/static/sass/*.scss", function () {
+        build_style();
+    });
 }
 
-exports.build_css = build_css;
-exports.build_sass_doc = build_sass_doc;
-exports.default = series(build_css(), build_sass_doc());
+function build_style_doc() {
+    var options = {
+        dest: 'docs/sassdoc'
+    }
+    return src('src/main/resources/static/sass/*.scss')
+        .pipe(sassdoc(options));
+}
+
+function run_project() {
+    return run("mvnw spring-boot:run").exec();
+}
+
+
+
+
+exports.build_style = build_style;
+exports.build_style_watch = build_style_watch;
+exports.build_style_doc = build_style_doc;
+exports.run = run_project;
+exports.default = series(build_style, run_project);
